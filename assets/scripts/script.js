fef3b53a00125e6205063c6dc40534eb1b279e3e
Logan Merchant <merchantclogan@gmail.com>
2020-08-22 00:36:09 -0600

    add initial quiz start

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,7 +6,7 @@ var introEl = document.querySelector("#intro");
 
 var questions = [
     {
-        question: "Which of the following languages provide a website with it's styling?",
+        question: "Which of the following is responsible for the way a page is styled?",
         option1: "HTML",
         option2: "CSS",
         option3: "Javascript",
@@ -15,8 +15,8 @@ var questions = [
     },
 ];
 
-var startQuiz = function(questions) {
-    var timeLeft = 10;
+var startTimer = function(questions) {
+    var timeLeft = 3;
     var timeInterval = setInterval(function() {
         if (timeLeft > 1) {
             timerEl.textContent = (timeLeft + " seconds left...");
@@ -32,14 +32,45 @@ var startQuiz = function(questions) {
             endGame();
         }
     }, 1000); 
-
-    // for (var i = 0; i < questions.length; i++) {
-    //     introEl.textContent = questions[i].question;
-    // };
 };
+
+var startQuiz = function() {
+    var buttonContainerEl = document.createElement('div');
+    buttonContainerEl.className = "btn-container";
+
+    var answerButtonEl1 = document.createElement('button');
+    var answerButtonEl2 = document.createElement('button');
+    var answerButtonEl3 = document.createElement('button');
+    var answerButtonEl4 = document.createElement('button');
+    answerButtonEl1.className = "btn";
+    answerButtonEl2.className = "btn";
+    answerButtonEl3.className = "btn";
+    answerButtonEl4.className = "btn";
+
+
+    mainEl.removeChild(startButtonEl);
+    mainEl.appendChild(buttonContainerEl);
+
+    for (var i = 0; i < questions.length; i++) {
+        introEl.textContent = questions[i].question;
+
+        buttonContainerEl.appendChild(answerButtonEl1);
+        answerButtonEl1.textContent = questions[i].option1;
+
+        buttonContainerEl.appendChild(answerButtonEl2);
+        answerButtonEl2.textContent = questions[i].option2;
+        
+        buttonContainerEl.appendChild(answerButtonEl3);
+        answerButtonEl3.textContent = questions[i].option3;
+
+        buttonContainerEl.appendChild(answerButtonEl4)
+        answerButtonEl4.textContent = questions[i].option4;
+    };
+}
 
 var endGame = function() {
-    console.log('hey');
+    
 };
 
+startButtonEl.addEventListener('click', startTimer);
 startButtonEl.addEventListener('click', startQuiz);
