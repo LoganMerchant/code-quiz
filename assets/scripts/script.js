a49c9a78ea40eeb7a999ab58a7b9a41d4fb28b98
Logan Merchant <merchantclogan@gmail.com>
2020-08-21 23:46:49 -0600

    start timer on start quiz click

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,23 +4,38 @@ var mainEl = document.querySelector("main");
 var introEl = document.querySelector("#intro");
 
 
-// var questions = [
-//     {
-//         question: "Which of the following languages provide a website with it's styling?",
-//         option1: "HTML",
-//         option2: "CSS",
-//         option3: "Javascript",
-//         option4: "JSON",
-//         answer: questions.option2,
-//     },
-// ];
+var questions = [
+    {
+        question: "Which of the following languages provide a website with it's styling?",
+        option1: "HTML",
+        option2: "CSS",
+        option3: "Javascript",
+        option4: "JSON",
+        // answer: questions.option2,
+    },
+];
 
 var startQuiz = function(questions) {
-    mainEl.removeChild(introEl);
-    mainEl.removeChild(startButtonEl);
-    
+    var timeLeft = 10;
+    var timeInterval = setInterval(function() {
+        if (timeLeft > 1) {
+            timerEl.textContent = (timeLeft + " seconds left.");
+            timeLeft--;
+        }
+        else if (timeLeft = 1) {
+            timerEl.textContent = (timeLeft + ' second left.');
+            timeLeft--;
+        }
+        else if (timeLeft < 1) {
+            timerEl.textContent = "";
+            clearInterval(timeInterval);
+            // endGame();
+        }
+    }, 1000); 
+
     for (var i = 0; i < questions.length; i++) {
-    }
-}
+        introEl.textContent = questions[i].question;
+    };
+};
 
 startButtonEl.addEventListener('click', startQuiz);
