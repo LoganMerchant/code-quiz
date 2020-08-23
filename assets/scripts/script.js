9f7b7c7ecfdeb8591a05a85731d4f7bd5ccfeefc
Logan Merchant <merchantclogan@gmail.com>
2020-08-22 19:12:03 -0600

    add clickedAnswerHandler to determine if user answer is correct

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,6 +5,8 @@ var titleEl = document.querySelector('.title')
 var infoEl = document.querySelector(".info");
 var startButtonEl = document.querySelector('#start-btn');
 var index = 0;
+var timeLeft = 30;
+var score = 0;
 
 var questionsObj = [
     {
@@ -13,7 +15,7 @@ var questionsObj = [
         choice2: "CSS",
         choice3: "Javascript",
         choice4: "JSON",
-        answer: this.choice2,
+        answer: '1',
     },
     {
         question: "What brackets are used to store properties in an array?",
@@ -21,12 +23,19 @@ var questionsObj = [
         choice2: '{ }',
         choice3: '( )',
         choice4: '< >',
-        answer: this.choice1,
+        answer: '0',
     },
+    {
+        question: "Guinness tastes good?",
+        choice1: "Yes",
+        choice2: "No",
+        choice3: "Eh",
+        choice4: "Maybe",
+        answer: '0',
+    }
 ];
 
 var startTimer = function() {
-    var timeLeft = 30;
     var timeInterval = setInterval(function() {
         if (timeLeft > 1) {
             timerEl.textContent = ('Time Left: ' + timeLeft + ' seconds');
@@ -100,13 +109,15 @@ var endQuiz = function() {
 
 var clickedAnswerHandler = function(event) {
     var quizAnswerConfirm = document.querySelector('.info');
-    var targetEl = event.target;
-    var answer = questionsObj[index].answer;
+    var eventTarget = event.target;
+    var targetEl = eventTarget.getAttribute('data-id');
 
-    if (targetEl.matches(answer)) {
+    if (targetEl === questionsObj[index].answer) {
         quizAnswerConfirm.textContent = "Correct!";
+        score++;
     } else {
         quizAnswerConfirm.textContent = 'Incorrect!';
+        timeLeft = (timeLeft - 3);
     }
 
     index++;
